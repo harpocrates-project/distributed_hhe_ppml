3ebce5d89079b8686181088693c4d0469c77c71f
jmz-b <jmz-b@users.noreply.github.com>
2025-03-04 15:54:21 +0100

    middleware: Move CSPService to own module

--- middleware/analyse.py
+++ middleware/analyse.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 
-import base64
-import grpc
-
-from hhe_pb2_grpc import CSPServiceStub
-from hhe_pb2 import DataFile, EncSymmetricDataMsg
-
-
-class CSPService:
-    def __init__(self, host):
-        self._channel = grpc.insecure_channel(host)
-        self._stub = CSPServiceStub(self._channel)
-
-    def evaluate_model_from_file(self, filename):
-        data_file = DataFile(filename=filename)
-        return self._stub.evaluateModelFromFile(data_file)
-
-    def close(self):
-        self._channel.close()
-
-    def __enter__(self):
-        return self
-              
-    def __exit__(self, exc_type=None, exc_value=None, traceback=None):
-        self.close()
-
+from csp import CSPService
 
 if __name__ == '__main__':
     import sys
---
+++ middleware/csp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import base64
+import grpc
+
+from hhe_pb2_grpc import CSPServiceStub
+from hhe_pb2 import DataFile, EncSymmetricDataMsg, EncSymmetricDataRecord
+
+
+class CSPService:
+    def __init__(self, host):
+        self._channel = grpc.insecure_channel(host)
+        self._stub = CSPServiceStub(self._channel)
+
+    def evaluate_model_from_file(self, filename):
+        return self._stub.evaluateModelFromFile(
+            DataFile(filename=filename)
+        )
+
+    def add_encrypted_data(self, record, patient_id):
+        return self._stub.addEncryptedData(
+            EncSymmetricDataMsg(
+                record=[EncSymmetricDataRecord(value=[r]) for r in record],
+                patientID=patient_id
+            )
+        )
+
+    def close(self):
+        self._channel.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type=None, exc_value=None, traceback=None):
+        self.close()
